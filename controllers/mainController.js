338beae2317251662bec306f56365e24e630c6fd
shiminglsm <shiming-lsm@hotmail.com>
2017-11-01 20:15:28 +0800

    choose carpark

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,16 +1,49 @@
 module.exports = function(app) {
     
+    var bodyParser = require('body-parser');
+    var urlencodedParser = bodyParser.urlencoded({extended:false});
     var firebase = require('firebase');
     var database = firebase.database(); 
-    
+    var carparkRef = database.ref('Carparks');
+    var lotsRef = database.ref('Lots');
+
     app.get('/carpark', function(req,res){
-        var carparkRef = database.ref('Carparks');
+
         carparkRef.on('value', function(snapshot){
             res.render('main', {carparks: snapshot.val()} );
         });
     });
 
-    app.post('/carpark', function(req,res){
-        res.redirect('checkin');
+    app.post('/carpark', urlencodedParser, function(req,res){
+        var carpark = req.body.carpark;
+        carparkRef.orderByChild('CarparkName').equalTo(carpark).on("child_added", function(snapshot){
+        if (snapshot.val().AvailableLots == 0) {
+            console.log('no lots');
+        }
+        else {
+            var carparkid = snapshot.val().CarparkID;
+            //lotsRef.orderByChild('CarparkID').equalTo(carparkid).on("child_added", function(snapshot){
+             //   for (var i in snapshot.val()) {
+             //       console.log(i);
+             //   }
+                
+            //});
+            lotsRef.on('value', function(snapshot) {
+                for (var key in snapshot.val()) {
+                    if (snapshot.val()[key]['CarparkID'] == carparkid && snapshot.val()[key]['Availability'] ==true) {
+                        //var serialnumber = snapshot.val()[key]['SerialNumber'];
+                        //var details = [{SerialNumber: serialnumber}];
+                        
+                        res.render('checkin', {AllocatedTime: new Date().getHours()+ ':' + new Date().getMinutes() + ':' + new Date().getSeconds(),
+                            LotID: snapshot.val()[key]['LotID'],
+                            SerialNumber: snapshot.val()[key]['SerialNumber']});
+                        break;
+                    }
+                }
+            });
+        }
+    });
+
+        
     });
 };
