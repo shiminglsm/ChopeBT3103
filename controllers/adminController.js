e531799e423eb36748e202e248d9f24d5a041201
shiminglsm <shiming-lsm@hotmail.com>
2017-11-03 19:13:37 +0800

    admin page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,8 +3,8 @@ module.exports = function(app) {
     var firebase = require('firebase');
     var database = firebase.database(); 
     
-    app.get('/admin/demographics', function(req,res){
-        res.render('admin-demo');
+    app.get('/admin/userloyalty', function(req,res){
+        res.render('admin-user');
     });
     app.get('/admin/carpark', function(req,res){
         res.render('admin-carpark');
