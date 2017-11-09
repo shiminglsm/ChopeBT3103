fddc189172d734a27019e77ecd220c266a9d7b10
sarahgui <31029131+sarahguii@users.noreply.github.com>
2017-11-09 11:34:09 +0800

    added codes to login and register
    BUT probably need sign out before everything will work
    
    how to make the login button "reactive" or call it for a function?

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,7 +2,31 @@ module.exports = function(app) {
 
     var bodyParser = require('body-parser');
     var urlencodedParser = bodyParser.urlencoded({extended:false});
-    
+    var firebase = require('firebase');
+    //how to call the login button here? 
+    app.submit = function() {
+        var email = app.email;
+        var password = app.password;
+        if (!email || !password) {
+          return console.log('email and password required');
+        }
+        // Sign in user
+        firebase.auth().signInWithEmailAndPassword(email, password)
+          .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log('signIn error', error);
+            // ...
+        });
+    };
+
+    // Listen to auth state changes
+    firebase.auth().onAuthStateChanged(function(user) {
+        app.user = user;
+        console.log('user', user);
+    });
+
     app.get('/', function(req,res){
         res.render('login');
     });
